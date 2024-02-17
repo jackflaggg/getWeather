4045d54fdaefd1893a63d76a49a6129f0046b2c2
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2024-02-17 22:57:47 +0500

    add app

---
+++ src/selectors/selector.js
@@ -0,0 +1,5 @@
+export const container = document.querySelector('.container');
+export const weatherBox = document.querySelector('.weather-box');
+export const search = document.querySelector('.search');
+export const weatherDetails = document.querySelector('.weather-details');
+export const city = document.querySelector('.search-box input').value;
